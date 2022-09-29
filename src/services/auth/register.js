841f8b64f114f5f97b4a74465a35910cde5b5759
MadonnaNassef <112638566+MadonnaNassef@users.noreply.github.com>
2022-09-29 18:45:57 +0200

    Update register.js

--- src/services/auth/register.js
+++ src/services/auth/register.js
@@ -1,17 +1,20 @@
 import bcrypt from 'bcrypt';
 import Users from '../../helpers/db/users.db.js';
 export function register(req, res) {
-	const { name, email, password, age } = req.body;
+	const { name, email, password, age,role } = req.body;
 	if (Users.some((user) => user.email === email)) {
 		return res.status(400).json({ message: 'Email already exists' });
 	}
 	const encryptedPassword = bcrypt.hashSync(password, 10);
+	if (role!="student"&& role!="teacher"){
+        return res.status(400).json({ message: 'must be student or teacher' });
 	const user = {
 		id: Users.length + 1,
 		name,
 		email,
 		password: encryptedPassword,
 		age,
+		role,
 	};
 	Users.push(user);
 	return res.json({
