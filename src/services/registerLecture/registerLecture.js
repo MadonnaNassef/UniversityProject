99e20f1dad1ab087221c04613fa708546fee789f
Yousef Ashraf <bosayousef981@gmail.com>
2022-10-02 20:12:03 +0200

    .

---
+++ src/services/registerLecture/registerLecture.js
@@ -0,0 +1,50 @@
+import Lecture from '../../helpers/db/lecture.db.js';
+import User from '../../helpers/db/users.db.js';
+import registeredLectures from '../../helpers/db/registeredLectures.db';
+import {
+	badRequestResponse,
+	notFoundResponse,
+	okResponse,
+} from '../../helpers/functions/ResponseHandler';
+export async function registerLecture(req, res, next) {
+	try {
+		const { lectureId, userId } = req.body;
+		const lecture = Lecture.find((lecture) => lecture.id == lectureId);
+		if (!lecture) {
+			return notFoundResponse(res, 'Lecture not found');
+		}
+		const user = User.find((user) => user.id == userId);
+		if (!user) {
+			return notFoundResponse(res, 'User not found');
+		}
+		if (user.role !== 'student') {
+			return badRequestResponse(
+				res,
+				'Only students can register to lectures',
+			);
+		}
+		if (
+			registeredLectures.some(
+				(lecReg) =>
+					lecReg.userId == userId && lecReg.lectureId == lectureId,
+			)
+		) {
+			return badRequestResponse(
+				res,
+				'User already registered to this lecture',
+			);
+		}
+		const registeredLecture = {
+			lectureId,
+			userId,
+		};
+		registeredLectures.push(registeredLecture);
+		return okResponse(
+			res,
+			'Lecture registered successfully',
+			registeredLecture,
+		);
+	} catch (err) {
+		next(err);
+	}
+}
